211405ac5f18619cba9df5eae53c9be2a71725ed
Tang <1984123951@qq.com>
2019-07-04 13:32:19 +0800

    SoulPermission权限： 1、支持多项权限同时请求 2、支持检查通知权限 3、支持系统权限页面跳转 4、支持debug模式

--- app/src/main/java/com/ranran/myapplication/shortcutsdynamic/MainAShortctivity.java
+++ app/src/main/java/com/ranran/myapplication/shortcutsdynamic/MainAShortctivity.java
@@ -1,5 +1,6 @@
 package com.ranran.myapplication.shortcutsdynamic;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
@@ -10,12 +11,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.qw.soul.permission.SoulPermission;
+import com.qw.soul.permission.bean.Permissions;
+import com.qw.soul.permission.callbcak.CheckRequestPermissionsListener;
 import com.ranran.myapplication.R;
 
+import java.security.acl.Permission;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,14 +38,28 @@ public class MainAShortctivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        SoulPermission.getInstance().checkAndRequestPermissions(
+                Permissions.build(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                //if you want do noting or no need all the callbacks you may use SimplePermissionsAdapter instead
+                new CheckRequestPermissionsListener() {
+                    @Override
+                    public void onAllPermissionOk(com.qw.soul.permission.bean.Permission[] allPermissions) {
+                        Toast.makeText(MainAShortctivity.this, allPermissions.length + "permissions is ok" +
+                                " \n  you can do your operations", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(com.qw.soul.permission.bean.Permission[] refusedPermissions) {
+                        Toast.makeText(MainAShortctivity.this, refusedPermissions[0].toString() +
+                                " \n is refused you can not do next things", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
         mListView = (ListView) findViewById(R.id.list);
         setupAdapter();
         mListView.setAdapter(mAdapter);
         setupShortcuts();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            mShortcutManager = getSystemService(ShortcutManager.class);
-            getNewShortcutInfo();
-        }
+
 
 
         mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -59,6 +79,41 @@ public class MainAShortctivity extends AppCompatActivity {
                 updItem(i);
             }
         });
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            mShortcutManager = getSystemService(ShortcutManager.class);
+            getNewShortcutInfo();
+        }
+    }
+
+    /**
+     * 动态添加三个
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+    private void getNewShortcutInfo() {
+        ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
+                .setShortLabel("Web site")
+                .setLongLabel("打开百度")
+                .setIcon(Icon.createWithResource(this, R.drawable.player))
+                .setIntent(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.baidu.com/")))
+                .build();
+        ShortcutInfo shortcut2 = new ShortcutInfo.Builder(this, "id2")
+                .setShortLabel("Web site")
+                .setLongLabel("我的Github")
+                .setIcon(Icon.createWithResource(this, R.drawable.player))
+                .setIntent(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/growuptang")))
+                .build();
+        ShortcutInfo shortcut3 = new ShortcutInfo.Builder(this, "id3")
+                .setShortLabel("Web site")
+                .setLongLabel("我的CSDN")
+                .setIcon(Icon.createWithResource(this, R.drawable.player))
+                .setIntent(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://blog.csdn.net/qq_33721320")))
+                .build();
+        mShortcutManager.setDynamicShortcuts(Arrays.asList(shortcut, shortcut2, shortcut3));
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N_MR1)
@@ -113,45 +168,16 @@ public class MainAShortctivity extends AppCompatActivity {
 
     private void setupAdapter() {
         List<String> list = new ArrayList<>(10);
-        list.add("亓斌");
-        list.add("齐彬");
-        list.add("乔杉");
+        list.add("杨");
+        list.add("老李");
+        list.add("老刘");
         list.add("张三");
         list.add("李四");
         list.add("王五");
-        list.add("狗蛋");
-        list.add("二狗子");
+        list.add("二哈");
+        list.add("毛驴");
         mAdapter = new Adapter();
         mAdapter.setDatas(list);
     }
 
-
-    /**
-     * 动态添加三个
-     */
-    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
-    private void getNewShortcutInfo() {
-        ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
-                .setShortLabel("Web site")
-                .setLongLabel("第一个")
-                .setIcon(Icon.createWithResource(this, R.drawable.player))
-                .setIntent(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.baidu.com/")))
-                .build();
-        ShortcutInfo shortcut2 = new ShortcutInfo.Builder(this, "id2")
-                .setShortLabel("Web site")
-                .setLongLabel("我的Github")
-                .setIcon(Icon.createWithResource(this, R.drawable.player))
-                .setIntent(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://github.com/growuptang")))
-                .build();
-        ShortcutInfo shortcut3 = new ShortcutInfo.Builder(this, "id3")
-                .setShortLabel("Web site")
-                .setLongLabel("我的CSDN")
-                .setIcon(Icon.createWithResource(this, R.drawable.player))
-                .setIntent(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://blog.csdn.net/qq_33721320")))
-                .build();
-        mShortcutManager.setDynamicShortcuts(Arrays.asList(shortcut, shortcut2, shortcut3));
-    }
 }
